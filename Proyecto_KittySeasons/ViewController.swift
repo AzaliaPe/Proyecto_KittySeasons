68a5a662cf362ed3e0d0872bf98b069335635b17
José Trejo <joseatrejos>
2022-09-08 11:00:27 -0700

    Tap

--- Proyecto_KittySeasons/ViewController.swift
+++ Proyecto_KittySeasons/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblOption: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func doTapSpring(_ sender: Any) {
+        lblOption.text = "Spring"
+    }
+    
+    @IBAction func doTapSummer(_ sender: Any) {
+        lblOption.text = "Summer"
+    }
+    
+    @IBAction func doTapFall(_ sender: Any) {
+        lblOption.text = "Fall"
+    }
+    
+    @IBAction func doTapWinter(_ sender: Any) {
+        lblOption.text = "Winter"
+    }
 }
 
